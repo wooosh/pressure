2a9b422b612569f6086553270f6ee99bfacd45c8
sfplayer247 <>
2019-10-10 02:22:23 +0000

    added tar decompression

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "bytes"
     "io"
     "log"
+    "strings"
 )
 
 func init() {
@@ -15,10 +16,17 @@ func init() {
 type DecompressionCheck func (f *os.File) bool
 type Decompressor func (f *os.File, target string)
 
+type CompressionCheck func (filename string) bool
+type Compressor func (filename string, target string)
+
 type ArchiveType struct {
     Name string
+
     CheckDecompress DecompressionCheck
     Decompress Decompressor
+
+    CheckCompression CompressionCheck
+    Compress Compressor
 }
 
 func check(e error) {
@@ -36,13 +44,20 @@ func main() {
     f, err := os.Open(os.Args[1])
     check(err)
 
+    var compressor Compressor
     for _, archiveType := range ArchiveIndex {
         if archiveType.CheckDecompress(f) {
             archiveType.Decompress(f, os.Args[2] + "/")
             return
+        } else if archiveType.CheckCompression(os.Args[2]) {
+            compressor = archiveType.Compress
         }
     }
-    log.Fatal("No decompressor available for file " + os.Args[1])
+    if compressor != nil {
+        compressor(os.Args[1], os.Args[2])
+    } else {
+        log.Fatal("No decompressor available for file " + os.Args[1])
+    }
 }
 
 // Generates a function that checks magic values at a certain offset
@@ -57,4 +72,29 @@ func checkMagic(offset int64, sequence []byte) DecompressionCheck {
     }
 }
 
+// Generates a function that checks file extension
+// We take a slice of strings because formats like zip have many different names (jar, epub, docx etc)
+func checkExt(extensions []string) CompressionCheck {
+    return func (filename string) bool {
+        for _, ext := range extensions {
+            // strings.HasSuffix is used instead of filepath.Ext because
+            // filepath.Ext will only capture the final dot, so formats
+            // like tar.gz won't be able to be detected
+            if strings.HasSuffix(filename, ext) {
+                return true
+            }
+        }
+        return false
+    }
+}
 
+func NoDecompressionCheck(f *os.File) bool {
+    return false
+}
+
+func NoCompressionCheck(f string) bool {
+    return false
+}
+
+func NoCompression(f string, target string) {}
+func NoDecompression(f *os.File, target string) {}
--- tar.go
+++ tar.go
@@ -8,6 +8,8 @@ import (
     "io"
     "path/filepath"
     "os"
+    "strings"
+    "log"
 )
 
 func init() {
@@ -18,6 +20,8 @@ func init() {
             []byte{0x75,0x73,0x74,0x61,0x72,0x00,0x30,0x30},
         ),
         untarWrap,
+        checkExt([]string{"tar"}),
+        tarWrap,
     })
     ArchiveIndex = append(ArchiveIndex, ArchiveType{
         "tar.gz",
@@ -26,7 +30,54 @@ func init() {
             []byte{0x1F, 0x8B},
         ),
         untargz,
+        NoCompressionCheck,
+        NoCompression,
    })
+}
+
+func tarWrap(f string, target string) {
+    tarFile(f, target, false)
+}
+
+func tarFile(f string, target string, gzip bool) {
+    archive, err := os.Create(target)
+    check(err)
+    defer archive.Close()
+
+    tw := tar.NewWriter(archive)
+
+    err = filepath.Walk(f, func(path string, info os.FileInfo, err error) error {
+        if err != nil  {
+            log.Println(err)
+            return nil
+        }
+        if !info.Mode().IsRegular() {
+            return nil
+        }
+
+        file, err := os.Open(path)
+        check(err)
+        defer file.Close()
+
+        header, err := tar.FileInfoHeader(info, info.Name())
+        check(err)
+
+        header.Name = strings.TrimPrefix(strings.Replace(path, f, "", -1), string(filepath.Separator))
+
+        if header.Name == "" {
+            return nil
+        }
+
+        err = tw.WriteHeader(header)
+        check(err)
+
+        _, err = io.Copy(tw, file)
+        check(err)
+
+        return nil
+    })
+    check(err)
+    check(tw.Close())
 }
 
 func untargz(f *os.File, target string) {
@@ -37,11 +88,14 @@ func untargz(f *os.File, target string) {
 }
 
 func untarWrap(f *os.File, target string) {
-    untar(f, target)
+        untar(f, target)
 }
 
 func untar(f io.Reader, target string) {
     tr := tar.NewReader(f)
+    
+    err := os.MkdirAll(target, os.ModePerm)
+    check(err)
 
     for {
         header, err := tr.Next()
@@ -51,7 +105,6 @@ func untar(f io.Reader, target string) {
             return
         }
         check(err)
-
         // Create the path for the next file to decompress
         fileTarget := filepath.Join(target + header.Name)
 
@@ -59,6 +112,7 @@ func untar(f io.Reader, target string) {
             err := os.MkdirAll(fileTarget, os.ModePerm)
             check(err)
         } else if header.Typeflag == tar.TypeReg { // Normal Files
+            os.MkdirAll(filepath.Dir(fileTarget), os.ModePerm)
             dst, err := os.Create(fileTarget)
             check(err)
             _, err = io.Copy(dst, tr)
